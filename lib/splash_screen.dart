03c7ad6cec05129e1c89d2e5bc4bbde7ce658c6b
Daco2020 <dacokim32@gmail.com>
2024-03-22 22:27:13 +0900

    feat: splash_screen 구현

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,14 +6,24 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/main');
+    });
+
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircularProgressIndicator(),
-            SizedBox(height: 20),
-            Text('로딩 중...'),
+            Image.asset('assets/penguin.jpg', width: 200, height: 200),
+            Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: const Text("뺑끼펭귄",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey)),
+            ),
           ],
         ),
       ),
