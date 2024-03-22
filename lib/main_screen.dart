baee7d86f1a4ef9dbcdaca761af526b93f1b383d
Daco2020 <dacokim32@gmail.com>
2024-03-22 22:27:28 +0900

    feat: main_screen appbar 구현

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,6 +10,17 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu, color: Colors.blueGrey),
+        title: const Text(
+          '뺑끼펭귄 나가신다! 길을 비켜라😤',
+          style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
